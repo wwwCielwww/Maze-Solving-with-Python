728cbf825b6932b61c4cd4385ea42731ad97aab7
__Cielll__ <u3576776@connect.hku.hk>
2020-11-01 17:35:11 +0800

    Update streamlit_ui.py

--- streamlit_ui.py
+++ streamlit_ui.py
@@ -14,7 +14,7 @@ img, marked = None, None
 if use_default:
     img = cv2.imread('sample.jpg')
 elif file is not None:
-    file_bytes = bytearray(file.read())
+    file_bytes = np.asarray(bytearray(file.read()), dtype=np.uint8)
     img = cv2.imdecode(file_bytes, 1)
 
 if img is not None:
